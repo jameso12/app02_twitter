80cbdf9e24d0d27dbf1c16b216bf33b02c4dacdb
jameso12 <jamesorodz@gmail.com>
2022-03-08 15:26:39 -0400

    made user model

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/User.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/User.kt
@@ -1,4 +1,19 @@
 package com.codepath.apps.restclienttemplate.models
 
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import org.json.JSONObject
+
 class User {
+    var name :String = ""
+    var screenName :String = ""
+    var profilePictureURL :String = ""
+    companion object {
+        fun fromJSON(jsonObj :JSONObject): User{
+            val user = User()
+            user.name = jsonObj.getString("name")
+            user.screenName = jsonObj.getString("screen_name")
+            user.profilePictureURL = jsonObj.getString("profile_image_url_https")
+            return user
+        }
+    }
 }
