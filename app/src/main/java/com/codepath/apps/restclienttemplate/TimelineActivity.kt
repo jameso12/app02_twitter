c2f2ad2131d853c52f7dcd3b3e886d373952cdf8
jameso12 <jamesorodz@gmail.com>
2022-03-08 14:12:31 -0400

    now we work on rv

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.kt
@@ -2,10 +2,13 @@ package com.codepath.apps.restclienttemplate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
+private const val TAG = "TimelineActivity"
 class TimelineActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_timeline)
+        Log.i(TAG, "Oncreate")
     }
 }
