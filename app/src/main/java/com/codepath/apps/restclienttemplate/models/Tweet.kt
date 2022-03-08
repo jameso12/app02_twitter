1396987bcbab1f58156c423d8ae9ce40929baf2d
jameso12 <jamesorodz@gmail.com>
2022-03-08 15:43:17 -0400

    start from vid 5

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.kt
@@ -1,4 +1,27 @@
 package com.codepath.apps.restclienttemplate.models
 
+import org.json.JSONArray
+import org.json.JSONObject
+
 class Tweet {
+    var body :String = ""
+    var dateCreated = ""
+    var user :User? = null
+    companion object{
+        fun fromJSON(jsonObj : JSONObject):Tweet{
+            val tweet = Tweet()
+            tweet.body = jsonObj.getString("text")
+            tweet.dateCreated = jsonObj.getString("created_at")
+            tweet.user = User.fromJSON(jsonObj.getJSONObject("user"))
+            return  tweet
+        }
+
+        fun fromJsonArrray(jsonArray: JSONArray):List<Tweet>{
+            val tweets = ArrayList<Tweet>()
+            for(i in 0..jsonArray.length()){
+                tweets.add(fromJSON(jsonArray.getJSONObject(i)))
+            }
+            return  tweets
+        }
+    }
 }
